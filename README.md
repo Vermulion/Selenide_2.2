2a2521e9afb8fa73717de8c2c02ff65e6ac1c75a
Viacheslav Ryabov <vermulion@gmail.com>
2022-10-18 01:15:44 +0300

    Appveyor was added

---
+++ README.md
@@ -0,0 +1,3 @@
+# Selenide_2.2
+Card delivery tests (date, visible, duration etc.)
+[![Build status](https://ci.appveyor.com/api/projects/status/r1e04cf7nctfj7uy?svg=true)](https://ci.appveyor.com/project/Vermulion/selenide-2-2)
---
+++ src/test/java/ru/netology/web/DeliveryCardTest.java
@@ -0,0 +1,43 @@
+package ru.netology.web;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+import static org.openqa.selenium.remote.tracing.EventAttribute.setValue;
+
+class DeliveryCardTest {
+
+    @Test
+    void  ShouldSendForm() {
+        open("http://localhost:9999");
+        $("input[placeholder='Город']").setValue("Москва");
+        $("span[class='menu-item__control']").click();
+    }
+}
+
+//    @Test
+//    void shouldRegisterByAccountNumberDOMModification() {
+//        open("http://localhost:9999");
+//        $$(".tab-item").find(exactText("По номеру счёта")).click();
+//        $("[name='number']").setValue("4055 0100 0123 4613 8564");
+//        $("[name='phone']").setValue("+792000000000");
+//        $$("button").find(exactText("Продолжить")).click();
+//        $(withText("Успешная авторизация")).shouldBe(visible, Duration.ofMillis(5000));
+//        $(byText("Личный кабинет")).shouldBe(visible, Duration.ofMillis(5000));
+//    }
+//
+//    @Test
+//    void shouldRegisterByAccountNumberVisibilityChange() {
+//        open("http://localhost:9999");
+//        $$(".tab-item").find(exactText("По номеру счёта")).click();
+//        $$("[name='number']").last().setValue("4055 0100 0123 4613 8564");
+//        $$("[name='phone']").last().setValue("+792000000000");
+//        $$("button").find(exactText("Продолжить")).click();
+//        $(withText("Успешная авторизация")).shouldBe(visible, Duration.ofSeconds(5));
+//        $(byText("Личный кабинет")).shouldBe(visible, Duration.ofSeconds(5));
+//    }
