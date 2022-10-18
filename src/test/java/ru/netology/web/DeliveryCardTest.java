d2b9026a43a3dc4b9be9b4a415319e5c19217393
Viacheslav Ryabov <vermulion@gmail.com>
2022-10-18 23:11:34 +0300

    Date test was added

--- src/test/java/ru/netology/web/DeliveryCardTest.java
+++ src/test/java/ru/netology/web/DeliveryCardTest.java
@@ -1,9 +1,14 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.ElementsCollection;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
@@ -12,11 +17,21 @@ import static org.openqa.selenium.remote.tracing.EventAttribute.setValue;
 
 class DeliveryCardTest {
 
+    private String deliveryDate = generateDate(3);
+
+    private String generateDate(int day) {
+        return LocalDate.now().plusDays(day).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
     @Test
     void  ShouldSendForm() {
+        Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
         $("input[placeholder='Город']").setValue("Москва");
         $("span[class='menu-item__control']").click();
+        $("input[placeholder='Дата встречи']").click();
+        $("input[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.SHIFT,Keys.HOME),Keys.BACK_SPACE);
+        $("input[placeholder='Дата встречи']").setValue(deliveryDate);
     }
 }
 
